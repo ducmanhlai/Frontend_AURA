14f74969710e6369679491e6776d9810c6a5b299
Duc Manh <ducmanhlai22@gmail.com>
2024-06-28 10:00:08 +0700

    add

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../styles/globals.css';
+import Home from './Home'
+export default function App({ Component, pageProps }) {
+  return <Home/>;
+}
--- pages/index.js
+++
@@ -1,5 +0,0 @@
-export default () => (
-    <div>
-      <p>Hello Next.js</p>
-    </div>  
-   )
